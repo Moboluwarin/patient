96007288f8bbf4811b05cad494e34b8baba65e61
Moboluwarin <kolaogunnaike@gmail.com>
2017-07-02 17:14:41 +0200

    create show and new doctor pages

--- app/controllers/doctors_controller.rb
+++ app/controllers/doctors_controller.rb
@@ -2,4 +2,22 @@ class DoctorsController < ApplicationController
   def index
     @doctors = Doctor.all
   end
+
+  def show
+    @doctor = Doctor.find(params[:id])
+  end
+
+  def new
+    @doctor = Doctor.new
+  end
+  def create
+    doctor_params = params.require(:doctor).permit(:name, :age)
+    #create an instance of doctor
+    @doctor = Doctor.new(doctor_params)
+    if @doctor.save
+      redirect_to@doctor
+    else
+      render "new"
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "doctors#index"
   get "doctors" => "doctors#index"
+  get "doctors/:new" => "doctors#new", as: :new_doctor
+  get "doctors/:id" => "doctors#show", as: :doctor
+  post "doctors" => "doctor#create"
 end
